0e1dd688f54ede4404bc71fac6f6a7fb9afbbeff
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-02-01 17:28:02 -0500

    Adding logic to display singed in or log in or sign up in application view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to products_url, notice: "Login Successful!"
     else
+      flash.now[:alert] = "Invalid login credentials!"
       render :new
     end
   end

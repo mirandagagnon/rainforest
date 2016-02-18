0c2b7019011bc02f24fbd3146c592d068d55d7c1
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-02-18 14:25:11 -0500

    add search on products

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,6 +19,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
+    redirect_to product_path(@product)
   end
 
   private
